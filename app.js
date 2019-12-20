5c0703c00056e103caa705431a3298044b170401
‘Juliano <julianoperins@gmail.com>
2019-12-19 19:56:50 -0500

    Finished UI,still need to clear it out

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // Init Github
 const github = new Github;
+// Init UI
+const ui = new UI;
 
 // Search input
 const searchUser = document.getElementById('searchUser');
 
-// Search input event listener 
+// Search input event listener
 searchUser.addEventListener('keyup', (e) => {
     // Get input text
     const userText = e.target.value;
@@ -18,11 +20,11 @@ searchUser.addEventListener('keyup', (e) => {
 
                 } else {
                     // Show profile
-
+                    ui.showProfile(data.profile);
                 }
             })
     } else {
         // Clear profile
 
     }
-})
+}); 
--- github.js
+++ github.js
@@ -7,10 +7,30 @@ class Github {
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-        const profileData = await profileResponse.json();
+        const profile = await profileResponse.json();
 
         return {
             profile
         }
     }
 }
+
+
+
+
+// class Github {
+//     constructor() {
+//         this.client_id = 'fa6439d734961ee997c4';
+//         this.client_secret = 'cecb213e2b0ae1690d702cca63df66d185f3dadd';
+//     }
+
+//     async getUser(user) {
+//         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+//         const profileData = await profileResponse.json();
+
+//         return {
+//             profile
+//         }
+//     }
+// }
