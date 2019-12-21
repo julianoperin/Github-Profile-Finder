ec131d06758fcb71382fd654a529862d0854aa7b
‘Juliano <julianoperins@gmail.com>
2019-12-20 20:59:50 -0500

    Worked on alert message

--- ui.js
+++ ui.js
@@ -1,15 +1,15 @@
 class UI {
-    constructor() {
-        this.profile = document.getElementById('profile');
-    }
-
-    showProfile(user) {
-        this.profile.innerHTML = `
+  constructor() {
+    this.profile = document.getElementById('profile');
+  }
+  // Display profile on UI
+  showProfile(user) {
+    this.profile.innerHTML = `
         <div class="card card-body mb-3">
           <div class="row">
             <div class="col-md-3">
-              <img class="img-fluid mb-2" src="${user.avatar_url}">
-              <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+              <img class="img-fluid mb-2 rounded" src="${user.avatar_url}">
+              <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4 mt-2">View Profile</a>
             </div>
             <div class="col-md-9">
               <span class="badge badge-primary mr-2">Public Repos: ${user.public_repos}</span>
@@ -29,5 +29,43 @@ class UI {
         <h3 class="page-heading mb-3">Latest Repos</h3>
         <div id="repos"></div>
       `;
+  }
+  // Show alert
+  showAlert(message, className) {
+    // Clear any remaining alerts
+    this.clearAlert();
+    //Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = className;
+    // Add text 
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.searchContainer');
+    //Get search box
+    const search = document.querySelector('.search');
+    // Insert alert
+    container.insertBefore(div, search);
+
+    // Timeout after 3 sec
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+
+  }
+
+  // Clear alert message 
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+
+    if (currentAlert) {
+      currentAlert.remove();
     }
+  }
+
+
+  //Clear profile
+  clearProfile() {
+    this.profile.innerHTML = '';
+  }
 }
